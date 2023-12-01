6cea7699de8cc2ac17b7cd471ebca188adfd8e20
NSimas <neliasimas@hotmail.com>
2023-11-30 22:17:26 -0300

    Começar pelo simples...

---
+++ forca01.js
@@ -0,0 +1,8 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log("==========Bem -vindo a jogo da força!!!==========");
